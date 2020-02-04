9453e5df2a05471aa61bcb92d7e4d75d35cec443
edison-cy-yang <edison.cy.yang@gmail.com>
2020-02-04 20:53:38 +0000

    Create, show, and index pages to show articles mapped to corresponding REST endpoints

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,28 @@
 class ArticlesController < ApplicationController
+
+  ##GET /articles
+  def index
+    @articles = Article.all
+  end 
+
+  ##GET /articles/:id articles#show
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
 
   end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to @article
+  end
+
+  ##Strong Parameter
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
